e598581f3c634fe9eea63a454a26c4b6a77b6dbc
Ken Leland III <kenleezle@gmail.com>
2013-06-17 15:33:55 -0400

    Added Bar Chart

--- networthprojector.js
+++ networthprojector.js
@@ -1,4 +1,15 @@
 var inputsliders;
+var ts_stock, ts_re;
+var inputs, results;
+
+var barchart_width = 500;
+var barchart_height = 500;
+var barchart_margin = 25;
+var bar_margin = 3;
+
+var barchart_x;
+var barchart_y;
+
 
 $(function () {
   function updateVisualizations(results) {
@@ -6,6 +17,7 @@ $(function () {
     /////////////////////
     // Add your function for updating visualizations
     /////////////////////
+    buildBarChart();
   }
 
   function slidecb(event,ui) {
@@ -39,8 +51,72 @@ $(function () {
   /////////////////////
   buildRawDataFeed();
   updateVisualizations(calculateResults());
+  buildBarChart();
 })
+function buildBarChart() {
+	bar_width = ((barchart_width-(2*barchart_margin))/(inputs.years+1))-(2*bar_margin);
+	barchart_x = d3.scale.linear()
+    	.domain([0, 1])
+    	.range([barchart_margin, barchart_margin+bar_width+(2*bar_margin)]);
+    
+	barchart_y = d3.scale.linear()
+    	.domain([0, results.totalnetworth])
+		.rangeRound([0,barchart_height-(barchart_margin*2)]);
+		
+	console.log("bar_width = "+bar_width);
+	d3.select("#barchart").remove();
+			
+	var chart = d3.select("#barchart_container").append("svg")
+        .attr("id", "barchart")
+        .attr("width", barchart_width)
+        .attr("height", barchart_height);
+    
 
+    chart.selectAll("stockColumn")
+        .data(ts_stock)
+        .enter()
+        .append("rect")
+        .attr("class", "stockColumn")
+        .attr("x", function (d, i) {
+            return barchart_x(i)+bar_margin;
+        })
+        .attr("width",  bar_width)
+        
+    chart.selectAll("realestateColumn")
+        .data(ts_re)
+        .enter()
+        .append("rect")
+        .attr("class", "realestateColumn")
+        .attr("x", function (d, i) {
+            return barchart_x(i)+bar_margin;
+        })
+        .attr("width", bar_width)
+    updateBarChart();
+}
+function updateBarChart() {
+    console.log("updateBarChart()");
+
+    d3.select("#barchart")
+        .selectAll(".stockColumn")
+        .data(ts_stock)
+        .attr("height", function (d) {
+            return barchart_y(d);
+        })
+        .attr("y", function (d,i) {
+            return barchart_height - barchart_margin - barchart_y(d+ts_re[i]);
+        });
+     d3.select("#barchart")
+        .selectAll(".realestateColumn")
+        .data(ts_re)
+        .attr("height", function (d) {
+            return barchart_y(d);
+        })
+        .attr("y", function (d,i) {
+            return barchart_height - barchart_margin - barchart_y(d);
+        });
+    
+    console.log("updateBarChart() exit");
+}
 function buildInputSlider(i,p) {
   var obj = {
     label: $($(document.createElement('label')).attr('for',i.name)).text(i.text) ,
@@ -58,7 +134,7 @@ function buildInputSlider(i,p) {
 }
 
 function calculateResults() {
-  var inputs = {};
+  inputs = {};
 
   for (var i = 0; i < inputsliders.length; i++) {
     inputs[inputsliders[i].name] = $(inputsliders[i].obj.slider).slider('option','value');
@@ -72,15 +148,37 @@ function calculateResults() {
   results.mortgagepayment =  inputs.homeprice * (1 - inputs.downpayment) * Math.pow((1 + monthlymortapr),numofmortpayments) * monthlymortapr / (Math.pow((1 + monthlymortapr), numofmortpayments) - 1);
   results.homeexpenses = (results.propertytaxes/12) + inputs.rentmaint + inputs.heatelec + results.mortgagepayment;
   results.monthlysavings =  inputs.income - inputs.expenses - results.homeexpenses;
-  results.homevalue =  inputs.homeprice * (Math.pow((1 + inputs.reapr),inputs.years));
-  results.realestatedebt = ((inputs.homeprice * (1 - inputs.downpayment) * Math.pow(1 + inputs.mortapr, inputs.years)) - (results.mortgagepayment * 12 * (Math.pow((1 + inputs.mortapr),inputs.years) - 1) / inputs.mortapr));
+  results.homevalue =  calculateRealEstateValue(inputs.years);
+  results.realestatedebt = calculateRealEstateDebt(inputs.years);
   results.realestateequity = results.homevalue - results.realestatedebt;
   results.initstock = inputs.savings - inputs.downpayment * inputs.homeprice;
-  results.stockequity = (results.initstock * Math.pow((1 + inputs.investapr), inputs.years) + Math.max(0,results.monthlysavings) * 12 * (Math.pow(1 + inputs.investapr,(inputs.years + 1)) - (1 + inputs.investapr)) / inputs.investapr);
+  results.stockequity = calculateStockEquity(inputs.years);
   results.totalnetworth = (results.realestateequity + results.stockequity);
   results.instock = 100 * results.stockequity / (results.totalnetworth);
   results.inrealestate = 100 * results.realestateequity / (results.totalnetworth);
   results.indebt = 100 * results.realestatedebt / (results.totalnetworth);
-
+  
+  populateBarChartData();
+  
   return results;
+}
+
+function populateBarChartData() {
+	ts_stock = [];
+	ts_re = [];
+	for (var i = 0; i <= inputs.years; i++) {
+		ts_stock[i] = calculateStockEquity(i);
+		ts_re[i] = calculateRealEstateValue(i) - calculateRealEstateDebt(i)
+	}	
+	console.log(ts_re);
+	console.log(ts_stock);
+}
+function calculateStockEquity(years) {
+  return (results.initstock * Math.pow((1 + inputs.investapr), years) + Math.max(0,results.monthlysavings) * 12 * (Math.pow(1 + inputs.investapr,(years + 1)) - (1 + inputs.investapr)) / inputs.investapr);	
+}
+function calculateRealEstateValue(years) {
+	return inputs.homeprice * (Math.pow((1 + inputs.reapr),years));
+}
+function calculateRealEstateDebt(years) {
+  return ((inputs.homeprice * (1 - inputs.downpayment) * Math.pow(1 + inputs.mortapr, years)) - (results.mortgagepayment * 12 * (Math.pow((1 + inputs.mortapr),years) - 1) / inputs.mortapr));
 }
